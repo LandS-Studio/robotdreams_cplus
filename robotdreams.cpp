68cf2e25557bd217e136a87803b3dbb7add1e9db
LandSStudio <35688813+LandS-Studio@users.noreply.github.com>
2024-08-27 21:07:31 +0300

    Update_13

--- robotdreams.cpp
+++ robotdreams.cpp
@@ -1,20 +1,43 @@
-﻿// robotdreams.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// robotdreams.cpp : This file contains the "main" function. This is where the execution of the programme starts and ends.
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float health;
+    float item_distance;
+    float item_impact;
+    float result_impact;
+
+    std::cout << "Enter character health: ";
+    std::cin >> health;
+    std::cout << "\n";
+
+    while (health > 0) {
+        std::cout << "Enter distance to damaging object: ";
+        std::cin >> item_distance;
+        std::cout << "Enter item impact: ";
+        std::cin >> item_impact;
+
+        if (item_distance < 100) {
+            result_impact = item_impact * (1 - item_distance / 100);
+            health += result_impact;
+
+            if (health < 1 && health > 0) {
+                health = 1;
+            }
+
+            std::cout << "\nResult item impact: " << result_impact << std::endl;
+            
+            if (health <= 0) {
+                std::cout << "Character is DEAD" << std::endl;
+            }
+            else {
+                std::cout << "Character health: " << health << "\n" << std::endl;
+            }
+        }
+    }
+
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
